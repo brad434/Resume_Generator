b4b32d24d0e319a918d01a4322b0e37a8259690a
Bradley Chery <bchery618@gmail.com>
2024-10-16 02:08:30 -0400

    updated files

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,12 +1,11 @@
 import Header from '@/components/custom/Header'
-import { UserButton } from '@clerk/clerk-react'
+// import { UserButton } from '@clerk/clerk-react'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <UserButton />
       Landing Screen
     </div>
   )
